378197a833189d0254e9b14f03ac0724f50f8e0b
Timofey Mukhin <muhintv@gmail.com>
2026-06-12 11:37:29 +0200

    added Not Found Page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,33 @@
 import cls from './NotFoundPage.module.scss'
+import notFoundImg from '../../shared/assets/404.png'
+import Container from "../../shared/ui/Container/Container.jsx";
+import {Link} from "react-router-dom";
+import {getRouteMain} from "../../shared/config/router/routes.js";
 
 const NotFoundPage = () => {
   return (
-    <div className={cls.NotFoundPage}>
-      
-    </div>
+    <main className={cls.NotFoundPage}>
+        <Container>
+            <div className={cls.content}>
+                <img
+                    src={notFoundImg}
+                    alt="404 Page Not Found"
+                    className={cls.image}
+                />
+
+                <h1>Page Not Found</h1>
+                <p>
+                    We&apos;re sorry, the page you requested could not be found.
+                    <br />
+                    Please go back to the homepage.
+                </p>
+
+                <Link to={getRouteMain()} className={cls.button}>
+                    Go Home
+                </Link>
+            </div>
+        </Container>
+    </main>
   );
 };
 
