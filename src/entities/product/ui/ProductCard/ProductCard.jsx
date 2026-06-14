b518a32e551274586b814e8b1b60dc6c9184f2cb
Timofey Mukhin <muhintv@gmail.com>
2026-06-14 15:03:04 +0200

    added hasDiscount to ProductCard

--- src/entities/product/ui/ProductCard/ProductCard.jsx
+++ src/entities/product/ui/ProductCard/ProductCard.jsx
@@ -5,26 +5,34 @@ import {API_URL} from "../../../../shared/config/api.js";
 
 const ProductCard = ({product}) => {
     const discount = Math.round((1 - product.discont_price / product.price) * 100)
-  return (
-    <Link
-        to={getRouteProduct(product.id)}
-        className={cls.ProductCard}
-    >
-        <div className={cls.ImageBox}>
-            <img src={`${API_URL}${product.image}`} alt={product.title} />
-            <span className={cls.discount}>-{discount}%</span>
-        </div>
-
-        <div className={cls.info}>
-            <h3 className={cls.title}>{product.title}</h3>
-            <div className={cls.priceBox}>
-                <span className={cls.price}>${product.discont_price}</span>
-                <span className={cls.oldPrice}>${product.price}</span>
+    const hasDiscount = product.discont_price !== null;
+    const currentPrice = hasDiscount ? product.discont_price : product.price;
+    return (
+        <Link
+            to={getRouteProduct(product.id)}
+            className={cls.ProductCard}
+        >
+            <div className={cls.ImageBox}>
+                <img src={`${API_URL}${product.image}`} alt={product.title}/>
+                {hasDiscount && (
+                    <span className={cls.discount}>-{discount}%</span>
+                )}
             </div>
-        </div>
 
-    </Link>
-  );
+            <div className={cls.info}>
+                <h3 className={cls.title}>{product.title}</h3>
+                <div className={cls.priceBox}>
+                    <span className={cls.price}> ${currentPrice || ''}</span>
+                    {hasDiscount && (
+                        <span className={cls.oldPrice}>
+                            ${product.price}
+                         </span>
+                    )}
+                </div>
+            </div>
+
+        </Link>
+    );
 };
 
 export default ProductCard;
